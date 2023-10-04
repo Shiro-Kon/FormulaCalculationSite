bcf2b26abccb328d4b3f46eaaa1c50446900368b
Andrey Yarmak <andreyyarmak777@gmail.com>
2023-10-04 11:25:35 +0300

    create 3 input of number and 1 input btn and a little fix header css

--- src/Components/PythagoreanTheorem/Pythagoras.tsx
+++ src/Components/PythagoreanTheorem/Pythagoras.tsx
@@ -3,7 +3,13 @@ type Props = {}
 
 const Pythagoras = (props: Props) => {
   return (
-    <div className='box'>Pythagoras</div>
+    <div className='box'>
+        <input type="number" name="a" id="a" placeholder='Введіть число a' />
+        <input type="number" name="b" id="b" placeholder='Введіть число b'/>
+        <input type="number" name="c" id="c" placeholder='Введіть число c'/>
+        <input type="text" placeholder='Відповідь :' readOnly  />
+        <input type="button" value="Очистити" />
+    </div>
   )
 }
 
