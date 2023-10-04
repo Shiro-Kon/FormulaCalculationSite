837fbfc6c43ab3679afbc22a9932268f99545a92
Shiro <andreyyarmak777@gmail.com>
2023-10-04 20:32:01 +0300

    fixed

--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import Button from '../Button/Button';
-import './Calculator.scss';
+import React, { useState, useEffect, useCallback } from "react";
+import Button from "../Button/Button";
+import "./Calculator.scss";
 
 const Calculator: React.FC = () => {
-  const [input, setInput] = useState<string>('');
+  const [input, setInput] = useState<string>("");
 
   const handleButtonClick = (value: string) => {
     setInput((prevInput) => prevInput + value);
@@ -14,14 +14,12 @@ const Calculator: React.FC = () => {
       const result: string = evaluateExpression(input);
       setInput(result);
     } catch (error) {
-      setInput('Error');
+      setInput("Error");
     }
   };
-  
-  
 
   const clearInput = () => {
-    setInput('');
+    setInput("");
   };
 
   const deleteLastDigit = () => {
@@ -31,42 +29,44 @@ const Calculator: React.FC = () => {
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
       const key = event.key;
-      if (key === 'Enter' || key === ' ') {
+      if (key === "Enter" || key === " ") {
         event.preventDefault();
-        key === ' ' ? clearInput() : calculateResult();
+        key === " " ? clearInput() : calculateResult();
       } else if (/^[0-9+\-*/.]$/.test(key)) {
         handleButtonClick(key);
-      } else if (key === 'Backspace') {
+      } else if (key === "Backspace") {
         deleteLastDigit();
       }
     },
     [handleButtonClick, clearInput, deleteLastDigit]
   );
-
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, [handleKeyPress]);
 
   const evaluateExpression = (expression: string) => {
-
-    throw new Error('Evaluation not implemented');
+    throw new Error("Evaluation not implemented");
   };
 
   return (
     <div className="calculator">
-      <input type="text" className='calc-input' value={input} readOnly />
+      <input type="text" className="calc-input" value={input} readOnly />
       <div className="button-grid">
         <div className="button-row">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, '/', '+', '-', '*'].map((label) => (
-            <Button key={label} label={label.toString()} onClick={() => handleButtonClick(label.toString())} />
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, "/", "+", "-", "*"].map((label) => (
+            <Button
+              key={label}
+              label={label.toString()}
+              onClick={() => handleButtonClick(label.toString())}
+            />
           ))}
         </div>
         <div className="button-row">
           <Button label="C" onClick={clearInput} />
-          <Button label="0" onClick={() => handleButtonClick('0')} />
+          <Button label="0" onClick={() => handleButtonClick("0")} />
           <Button label="=" onClick={calculateResult} />
           <Button label="←" onClick={deleteLastDigit} />
         </div>
